935d5c68641b363a2152db693f538b92c976327d
Kaushigithub <kypancholi2@gmail.com>
2025-07-03 21:37:57 +0530

    Added Post API

--- chat/api.py
+++ chat/api.py
@@ -1,5 +1,6 @@
 from ninja import NinjaAPI
-from .models import Message
+from .models import Message, Room
+from django.contrib.auth.models import User
 from pydantic import BaseModel
 from typing import List
 from datetime import datetime
@@ -13,6 +14,14 @@ class MessageSchema(BaseModel):
     content: str
     timestamp: datetime
 
+
+class MessageCreateSchema(BaseModel):
+    username: str
+    room: str
+    content: str
+
+# Get all messages
+
 @api.get("/messages", response=List[MessageSchema])
 def get_messages(request):
     return [
@@ -24,3 +33,25 @@ def get_messages(request):
             timestamp=m.timestamp
         ) for m in Message.objects.select_related("user", "room").all()
     ]
+
+
+# Post new message
+
+@api.post("/messages", response=MessageSchema)
+def create_message(request, payload: MessageCreateSchema):
+    user = User.objects.get(username=payload.username)
+    room, _ = Room.objects.get_or_create(name=payload.room)
+
+    message = Message.objects.create(
+        user=user,
+        room=room,
+        content=payload.content
+    )
+
+    return MessageSchema(
+        id=message.id,
+        user=message.user.username,
+        room=message.room.name,
+        content=message.content,
+        timestamp=message.timestamp
+    )
